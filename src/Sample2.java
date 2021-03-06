3dcede76dbb31d2d0e1fc59d73ff80723ee4e6a4
MohanDarure <mohandarure436@gmail.com>
2021-03-06 12:56:08 -0500

    my commite

---
+++ src/Sample2.java
@@ -0,0 +1,10 @@
+public class Sample2 {
+    public static void main(String[] args) {
+      Sample2 demo = new Sample2();
+      demo.display();
+    }
+    public void display(){
+        System.out.println("Mohan Darure");
+    }
+
+}
